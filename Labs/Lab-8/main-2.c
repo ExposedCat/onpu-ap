8ecf166b460e85fdbf837aabb93e5c8fe031ac28
ExposedCat <artem13.prokop@gmail.com>
2021-10-14 21:29:27 +0300

    Add 2nd task of 8th lab

--- Labs/Lab-8/main-2.c
+++ Labs/Lab-8/main-2.c
@@ -1,33 +1,40 @@
-#include <math.h>
 #include <stdio.h>
-
-unsigned long factorial(unsigned a) {
-    if (a == 0) {
-        return 1;
-    }
-    return a * factorial(a - 1);
-}
-
 void main() {
-    unsigned n;
-    double sum = 0;
+    unsigned rows = 15;
+    unsigned columns;
+    printf("Input columns number: ");
+    scanf("%d", &columns);
 
-    printf("┌─────────────┐\n");
-    printf("│  N = ");
-    scanf("%d", &n);
-    printf("└─────────────┘\n");
+    double matrix[rows][columns];
+    const unsigned size = rows * columns / 2;
+    double array[size];
+    unsigned index = 0;
 
-    for (int k = 1; k <= n; ++k) {  // Not unsigned int, because C is a piece of shit.
-        if (k == 3) {
-            continue;
+    printf("\nInput matrix %dx%d:\n", rows, columns);
+    for (unsigned row = 0; row < rows; ++row) {
+        for (unsigned column = 0; column < columns; ++column) {
+            scanf("%lf", &matrix[row][column]);
         }
-        double q = pow(k - 3, k - 5) * (k + 7) / factorial(k);
-        sum += q;
     }
 
-    printf("┌─────────────┐\n");
-    printf("│  Sum = %5.2lf│\n", sum);
-    printf("└─────────────┘");
+    for (unsigned row = 0; row < 3; ++row) {
+        // for instead of assignment because C is a piece of shit
+        for (unsigned column = 0; column < columns; ++column) {
+            double temp = matrix[rows - 3 + row][column];
+            matrix[rows - 3 + row][column] = matrix[row][column];
+            matrix[row][column] = temp;
+        }
+    }
 
     printf("\n");
+
+    for (unsigned row = 0; row < rows; ++row) {
+        printf("( ");
+        for (unsigned column = 0; column < columns; ++column) {
+            printf("%5.2lf ", matrix[row][column]);
+        }
+        printf(")\n");
+    }
+
+    printf("\n\n");
 }
