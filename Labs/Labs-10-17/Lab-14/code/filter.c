0343674af0b63d68fa57b2876107e936178e56ce
ExposedCat <artem13.prokop@gmail.com>
2021-11-06 16:00:14 +0200

    Add 2nd part of 14-th lab

--- Labs/Labs-10-17/Lab-14/code/filter.c
+++ Labs/Labs-10-17/Lab-14/code/filter.c
@@ -1,1 +1,38 @@
+#include "filter.h"
+
 #include <stdio.h>
+
+#include "entities.h"
+
+void scanAndFilter(int records_number, char* filename) {
+    char value;
+    printf("Input value:\nV - Voltage\nA - Electric current\nL - Length\nT - Temperature\nH - Humidity\n> ");
+    scanf("%c", &value);
+    int guarantee;
+    printf("Input guarantee in years:\n> ");
+    scanf("%d", &guarantee);
+
+    struct Instrument instruments[records_number];
+    FILE* database = fopen(filename, "rb");
+    fread(&instruments, sizeof(struct Instrument), records_number, database);
+    int output = 0;
+    for (int i = 0; i < records_number; ++i) {
+        struct Instrument instrument = instruments[i];
+        if (instrument.guarantee >= guarantee) {
+            int value_found = 0;
+            for (int i = 0; i < 100; ++i) {
+                if (instrument.values[i].name == value) {
+                    value_found = 1;
+                    break;
+                }
+            }
+            if (value_found) {
+                printf("%s\n", instrument.name);
+                output = 1;
+            }
+        }
+    }
+    if (!output) {
+        printf("No records found.");
+    }
+}
--- Labs/Labs-10-17/Lab-14/code/main.c
+++ Labs/Labs-10-17/Lab-14/code/main.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 
+#include "filter.h"
 #include "initialize.h"
 
 void main() {
     int records_number;
-    printf("Input records number: ");
+    printf("Input recordsg number: ");
     scanf("%d", &records_number);
     scanAndWrite(records_number, "gi-database");
+    scanAndFilter(records_number, "gi-database");
+    printf("\n");
 }
